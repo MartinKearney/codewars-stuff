e5e4e36ad35598e8100447b230127c3738db29c3
MartinKearney <martin-kearney@hotmail.co.uk>
2022-02-24 00:15:14 +0000

    Roman Numerals Kata

--- 6-kyu/Roman-Numerals-Encoder.js
+++ 6-kyu/Roman-Numerals-Encoder.js
@@ -12,13 +12,11 @@ const encode = (num) => {
     codeString += codes[i].repeat(plurality);
   };
 
-  for (let i = 0; i < codes.length; i++) {
-    if (i === 0) {
-      // Special case for 1000s
-      updateCodeString(num, i);
-      r = num % units[i];
-      continue;
-    }
+  // Special case for 1000s
+  updateCodeString(num, 0);
+  r = num % units[0];
+
+  for (let i = 1; i < codes.length; i++) {
     if (i % 2 !== 0) {
       if (r >= 0.9 * units[i - 1]) {
         codeString += codes[i + 1] + codes[i - 1];
@@ -36,83 +34,73 @@ const encode = (num) => {
         r %= units[i];
       }
     }
+    if (r === 0) {
+      break;
+    }
   }
-
-  //   // Thousands
-  //   const nThousands = Math.floor(num / 1000);
-  //   const Thousands = 'M'.repeat(nThousands);
-  //   r = num % 1000;
-  //   codeString += Thousands;
-
-  //   // Five Hundreds
-  //   if (r >= 0.9 * 1000) {
-  //     codeString += 'CM';
-  //     r %= 0.9 * 1000;
-  //   } else {
-  //     const nFiveHundreds = Math.floor(r / 500);
-  //     const FiveHundreds = 'D'.repeat(nFiveHundreds);
-  //     r %= 500;
-  //     codeString += FiveHundreds;
-  //   }
-
-  //   // Hundreds
-  //   if (r >= 0.8 * 500) {
-  //     codeString += 'CD';
-  //     r %= 0.8 * 500;
-  //   } else {
-  //     const nHundreds = Math.floor(r / 100);
-  //     const Hundreds = 'C'.repeat(nHundreds);
-  //     r %= 100;
-  //     codeString += Hundreds;
-  //   }
-
-  //   // Fifties
-  //   if (r >= 0.9 * 100) {
-  //     codeString += 'XC';
-  //     r %= 0.9 * 100;
-  //   } else {
-  //     const nFifties = Math.floor(r / 50);
-  //     const Fifties = 'L'.repeat(nFifties);
-  //     r %= 50;
-  //     codeString += Fifties;
-  //   }
-
-  //   // Tens
-  //   if (r >= 0.8 * 50) {
-  //     codeString += 'XL';
-  //     r %= 0.8 * 50;
-  //   } else {
-  //     const nTens = Math.floor(r / 10);
-  //     const Tens = 'X'.repeat(nTens);
-  //     r %= 10;
-  //     codeString += Tens;
-  //   }
-
-  //   // Fives
-  //   if (r >= 0.9 * 10) {
-  //     codeString += 'IX';
-  //     r %= 0.9 * 10;
-  //   } else {
-  //     const nFives = Math.floor(r / 5);
-  //     const Fives = 'V'.repeat(nFives);
-  //     r %= 5;
-  //     codeString += Fives;
-  //   }
-
-  //   // Ones
-  //   if (r >= 0.8 * 5) {
-  //     codeString += 'IV';
-  //     r %= 0.8 * 5;
-  //   } else {
-  //     const nOnes = Math.floor(r / 1);
-  //     const Ones = 'I'.repeat(nOnes);
-  //     codeString += Ones;
-  //   }
-
   return codeString;
 };
 
-// console.log(encode(3869));
-// console.log(encode(900));
 console.log(encode(1000));
 console.log(encode(1973));
+
+// Turns out my solution is not the longest, but not far off!
+// Should have thought to have done it via a mapping which included 900 etc
+// Solutions of others:
+// 1.
+function solution(number) {
+  var roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  var ans = '';
+  while (number > 0) {
+    for (a in roman) {
+      if (roman[a] <= number) {
+        ans += a;
+        number -= roman[a];
+        break;
+      }
+    }
+  }
+  return ans;
+}
+
+// 2.
+function solution(number) {
+  let map = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    },
+    ans = '';
+  for (let key in map) {
+    while (number >= map[key]) {
+      ans += key;
+      number -= map[key];
+    }
+  }
+  return ans;
+}
