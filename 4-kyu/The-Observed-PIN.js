43831a92c433c19020d71f64147d0b7501cf2ca6
MartinKearney <martin-kearney@hotmail.co.uk>
2022-03-07 14:26:38 +0000

    Kata completed

--- 4-kyu/The-Observed-PIN.js
+++ 4-kyu/The-Observed-PIN.js
@@ -19,28 +19,30 @@ const getPINs = (observed) => {
     return pinVariables[0];
   }
 
-  const joinedArray = (arr1, arr2) =>
-    arr1.map((elt1) => arr2.map((elt2) => elt1 + elt2)).flat();
+  // need a new function here as codewars is running a version
+  // of node which doesn't support flat() :(
+  // const joinedArray = (arr1, arr2) =>
+  //   arr1.map((elt1) => arr2.map((elt2) => elt1 + elt2)).flat();
+
+  const joinedArray = (arr1, arr2) => {
+    const newArray = [];
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        newArray.push(arr1[i] + arr2[j]);
+      }
+    }
+    return newArray;
+  };
 
   let pins = [];
 
-  // for (let i = 0; i < pinVariables.length - 1; i++) {
-  //   i === 0
-  //     ? (pins = joinedArray(pinVariables[0], pinVariables[1]))
-  //     : (pins = joinedArray(pins, pinVariables[i + 1]));
-  // }
-
   for (let i = 0; i < pinVariables.length - 1; i++) {
-    if (i === 0) {
-      pins = joinedArray(pinVariables[0], pinVariables[1]);
-      console.log('i=0', pins);
-    } else {
-      pins = joinedArray(pins, pinVariables[i + 1]);
-      console.log(`i=${i} ${pins}`);
-    }
+    i === 0
+      ? (pins = joinedArray(pinVariables[0], pinVariables[1]))
+      : (pins = joinedArray(pins, pinVariables[i + 1]));
   }
 
-  return [...pins];
+  return pins;
 };
 
 console.log(getPINs('123'));
