e0b07f720777c116c46a14ea158a852f03fef678
MartinKearney <martin-kearney@hotmail.co.uk>
2022-03-09 01:17:59 +0000

    Matrix-Determinant final2

--- 4-kyu/Matrix-Determinant.js
+++ 4-kyu/Matrix-Determinant.js
@@ -48,3 +48,19 @@ m2 = [
 // console.log(determinant([[1]]));
 // console.log(determinant(m1));
 console.log(determinant(m2));
+
+// Top other solution
+function determinant(m) {
+  if (m.length == 0) return 0;
+  if (m.length == 1) return m[0][0];
+  if (m.length == 2) return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+  if (m.length > 2) {
+    return m.reduce((prev, curr, i, arr) => {
+      let miniArr = arr
+        .slice(0, i)
+        .concat(arr.slice(i + 1))
+        .map((item) => item.slice(1));
+      return prev + (i % 2 == 0 ? 1 : -1) * curr[0] * determinant(miniArr);
+    }, 0);
+  }
+}
